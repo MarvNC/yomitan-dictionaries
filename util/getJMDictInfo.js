fd4ca9dfa87f95d5e003e51bdfd2c5e9de80b3e5
MarvNC <rotrobmin@gmail.com>
2022-08-22 02:16:33 -0700

    jmdict read, deinflector get

--- util/getJMDictInfo.js
+++ util/getJMDictInfo.js
@@ -1,25 +1,50 @@
 const fs = require('fs');
 const jszip = require('jszip');
-const jmdictPath = '';
+const jmdictPath = './util/jmdict_english.zip';
+const termBankRegex = /term_bank_\d+\.json/;
 
 let JMDictData;
 
 async function getJMDictData() {
   if (JMDictData) return JMDictData;
 
-  const jmdict = JSON.parse(fs.readFileSync(jmdictPath));
-  for (const entry of jmdict) {
+  const allEntries = [];
 
+  const jmdictZip = await fs.promises.readFile(jmdictPath);
+  const jmdict = await jszip.loadAsync(jmdictZip);
+  for (const fileName of Object.keys(jmdict.files)) {
+    if (termBankRegex.test(fileName)) {
+      const termBank = await jmdict.files[fileName].async('string');
+      const data = JSON.parse(termBank);
+      allEntries.push(...data);
+    }
   }
+
+  JMDictData = {};
+  for (const entry of allEntries) {
+    const [term, reading, tags, deinflectors, popularity, sequence, bigTags] = entry;
+    JMDictData[(term, reading)] = {
+      reading,
+      tags,
+      deinflectors,
+      popularity,
+      sequence,
+      bigTags,
+    };
+  }
+  return JMDictData;
 }
 
-function getDeinflectors(term, reading) {
-  
+async function getDeinflectors(term, reading) {
+  const jmdict = await getJMDictData();
+  const entry = jmdict[(term, reading)];
+  if (!entry) {
+    throw new Error(`No JMDict entry for ${(term, reading)}`);
+  }
+  return entry.deinflectors;
 }
 
-function getKanjiReadings(kanji) {
-  
-}
+function getKanjiReadings(kanji) {}
 
 module.exports = {
   getDeinflectors,
