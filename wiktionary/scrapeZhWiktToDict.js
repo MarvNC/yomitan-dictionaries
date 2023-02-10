33f8de100bb944badcebf9c8da00d4d27932e5bf
MarvNC <rotrobmin@gmail.com>
2023-02-09 17:37:59 -0800

    base

---
+++ wiktionary/scrapeZhWiktToDict.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+const saveDict = require('../util/saveDict');
+const writeJson = require('../util/writeJson');
+const { getURL, wait } = require('../util/scrape.js');
+
+const data = require('./zhwiktionaryKanji.json');
+
+(async () => {
+  
+})();
