cbe15bb0d0e066361ef1548d6cb396194a62d602
MarvNC <rotrobmin@gmail.com>
2023-04-27 09:41:14 -0700

    complete scraping summaries code

--- nico-pixiv-dict/scrapePixiv.js
+++ nico-pixiv-dict/scrapePixiv.js
@@ -5,25 +5,60 @@ const saveDict = require('../util/saveDict');
 const writeJson = require('../util/writeJson');
 
 const folderPath = './nico-pixiv-dict/';
-const saveMetaJsonPath = folderPath + 'pixivMeta.json';
-const saveDataJsonPath = folderPath + 'pixivData.json';
+const saveSummariesJsonPath = folderPath + 'pixivSummaries.json';
+const saveArticlesJsonPath = folderPath + 'pixivArticles.json';
 
-const WAIT_MS = 1000;
+const WAIT_MS = 000;
 
 const domain = 'https://dic.pixiv.net/';
 const categryPath = 'category/';
 const articlePath = 'a/';
-const jsonPath = (page) => `?json=1&page=${page}`;
+const pageJsonPath = (page) => `?json=1&page=${page}`;
 
-let articles = {};
+const COUNT_PER_PAGE = 12;
+
+let articlesListSummaries = {};
 let articleData = {};
 (async function () {
   // const testJson = await getJSON(
   //   'https://dic.pixiv.net/category/アニメ?json=1&page=9068'
   // );
   const categoryURLs = await getListOfCategoryURLs();
-  const articles = await getListOfArticles(categoryURLs);
+  const articlesList = await getListOfArticles(categoryURLs);
+  const articlesSummaries = await getArticlesSummaries(articlesList);
 })();
+
+/**
+ * Gets article summaries from the list of articles using the json api
+ * @returns {Promise<Object>} list of article summaries
+ */
+async function getArticlesSummaries() {
+  console.log('Getting article summaries');
+  const startTime = Date.now();
+  const remainingArticlesKeys = Object.keys(articlesListSummaries).filter(
+    (key) => !articlesListSummaries[key]
+  );
+  for (let i = 0; i < remainingArticlesKeys.length; i++) {
+    const listURL = remainingArticlesKeys[i];
+    if (articlesListSummaries[listURL]) {
+      continue;
+    }
+    const { articles } = await getJSON(listURL);
+    articlesListSummaries[listURL] = articles;
+
+    const remaining = remainingArticlesKeys.length - i;
+    const timeElapsed = Date.now() - startTime;
+    const timePerArticle = timeElapsed / (i + 1);
+    const timeRemaining = remaining * timePerArticle;
+    const expectedEndTime = new Date(Date.now() + timeRemaining).toLocaleString();
+
+    console.log(
+      `Got ${listURL}, ${remaining} remaining, expected ${expectedEndTime}`
+    );
+    await wait(WAIT_MS);
+  }
+  await writeJson(articlesListSummaries, saveSummariesJsonPath);
+}
 
 /**
  * Gets a list of all articles on pixiv
@@ -31,18 +66,33 @@ let articleData = {};
  */
 async function getListOfArticles(categoryURLs) {
   console.log('Getting list of articles');
-  const categories = {};
   // check saved json, if not populate json with all json paths
   try {
-    const metaFile = await fs.readFile(saveMetaJsonPath);
-    categories = JSON.parse(metaFile);
-    console.log(`Loaded ${Object.keys(categories).length} categories from ${saveMetaJsonPath}`);
+    const categoriesFile = await fs.readFile(saveSummariesJsonPath);
+    articlesListSummaries = JSON.parse(categoriesFile);
+    console.log(
+      `Loaded ${Object.keys(articlesListSummaries).length} categories from ${saveSummariesJsonPath}`
+    );
   } catch (error) {
-    console.log(`No saved ${saveMetaJsonPath}, starting from scratch`);
+    console.log(`No saved ${saveSummariesJsonPath}, starting from scratch`);
+
+    let totalCount = 0;
     for (const categoryURL of categoryURLs) {
-      const { meta } = await getJSON(categoryURL + jsonPath(1));
-      const { all_count, count } = meta;
+      const { meta } = await getJSON(categoryURL + pageJsonPath(1));
+      const { all_count } = meta;
+      totalCount += all_count;
+      const categoryPageCount = Math.ceil(all_count / COUNT_PER_PAGE);
+      for (let i = 1; i <= categoryPageCount; i++) {
+        articlesListSummaries[categoryURL + pageJsonPath(i)] = false;
+      }
+      console.log(`Added ${categoryPageCount} pages from ${categoryURL}`);
     }
+    console.log(
+      `Saving ${
+        Object.keys(articlesListSummaries).length
+      } categories to ${saveSummariesJsonPath}, total of ${totalCount} articles`
+    );
+    await writeJson(articlesListSummaries, saveSummariesJsonPath);
   }
 }
 
@@ -56,3 +106,13 @@ async function getListOfCategoryURLs() {
   const hrefs = [...categories].map((a) => domain + categryPath + a.title);
   return hrefs;
 }
+
+// save on ctrl c
+process.on('SIGINT', async () => {
+  console.log('Saving...');
+  if (articlesListSummaries) {
+    console.log('Saving data...');
+    await writeJson(articlesListSummaries, saveSummariesJsonPath);
+  }
+  process.exit(0);
+});
