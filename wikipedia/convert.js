a4980d8ea753f1917d451ef71f396e122f03aa41
MarvNC <rotrobmin@gmail.com>
2023-12-16 16:19:53 -0800

    Complete wikipedia convert

--- wikipedia/convert.js
+++ wikipedia/convert.js
@@ -1,12 +1,22 @@
 const fs = require('fs');
+const JSZip = require('jszip');
 const path = require('path');
 const readline = require('readline');
+const TERMS_PER_JSON = 10000;
+
+const outputZipName = '[Mono Encyclopedia] Wikipedia.zip';
 
 (async () => {
   // Read argument for file path
   const filePath = process.argv[2];
   if (!filePath) {
     console.error('Please provide a file path');
+    return;
+  }
+
+  const version = process.argv[3];
+  if (!version) {
+    console.error('Please provide a version');
     return;
   }
 
@@ -17,11 +27,70 @@ const readline = require('readline');
     crlfDelay: Infinity,
   });
 
-  for await (const line of rl) {
-    const termInformation = parseLineToDefinition(line);
-    // Process each line here
-  }
+  await makeDict(rl, version);
 })();
+
+/**
+ * @param {readline.Interface} readLineInterface
+ * @param {string} version
+ */
+async function makeDict(readLineInterface, version) {
+  const outputZip = new JSZip();
+
+  let termBank = [];
+
+  let termBankCounter = 1;
+
+  /**
+   * Saves an object to the zip as a json file.
+   * @param {object} object
+   * @param {string} fileName
+   */
+  function saveToZip(object, fileName) {
+    outputZip.file(fileName, JSON.stringify(object));
+  }
+
+  for await (const line of readLineInterface) {
+    // Chunk the term bank into separate json term banks
+    if (termBank.length >= TERMS_PER_JSON) {
+      saveToZip(termBank, `term_bank_${termBankCounter}.json`);
+      console.log(`Wrote term_bank_${termBankCounter}.json`);
+      termBank = [];
+      termBankCounter++;
+    }
+
+    const termInformation = parseLineToDefinition(line);
+
+    termBank.push(termInformation);
+  }
+
+  saveToZip(termBank, `term_bank_${termBankCounter}.json`);
+
+  const index = {
+    title: `Wikipedia [${version}]`,
+    revision: `wikipedia_${new Date().toISOString()}`,
+    format: 3,
+    url: 'https://ja.wikipedia.org/',
+    description: `Wikipedia short abstracts from the DBPedia dataset available at https://databus.dbpedia.org/dbpedia/text/short-abstracts.
+Created with https://github.com/MarvNC/yomichan-dictionaries`,
+    author: 'Wikipedians, DBPedia, Marv',
+    attribution: 'Wikipedia',
+  };
+  saveToZip(index, 'index.json');
+
+  // save zip
+  console.log('Saving zip...');
+  outputZip
+    .generateAsync({
+      type: 'nodebuffer',
+      compression: 'DEFLATE',
+      compressionOptions: { level: 9 },
+    })
+    .then((content) => {
+      fs.writeFileSync(path.join(__dirname, outputZipName), content);
+      console.log(`Writing ${outputZipName}`);
+    });
+}
 
 /**
  *
@@ -35,16 +104,37 @@ function parseLineToDefinition(line) {
   let term = resource.split('.dbpedia.org/resource/').pop();
   term = decodeURIComponent(term);
 
-  console.log(term, definition);
+  /**
+   * @type {import('../types').DetailedDefinition}
+   */
+  const definitionArray = definition.split('\\n').map((line) => line.trim());
 
   const bracketRegex = /[(（]([^)）]*)/g;
-  const bracketMatches = definition.match(bracketRegex);
+  const bracketMatches = bracketRegex.exec(definition);
   let reading = '';
-  if (bracketMatches) {
+  if (bracketMatches?.length >= 1) {
     const bracketContent = bracketMatches[1];
-    console.log(bracketContent);
-    const readingRegex = /[^、]+/g;
+    reading = parseReadingFromBrackets(bracketContent);
   }
 
-  debugger;
+  return [term, reading, '', '', 0, definitionArray, 0, ''];
+}
+
+/**
+ * @param {string} bracketContent
+ * @returns {string}
+ */
+function parseReadingFromBrackets(bracketContent) {
+  if (!bracketContent) return '';
+
+  const commaRegex = /,|、/g;
+
+  const reading = bracketContent.split(commaRegex)[0];
+
+  if (reading.includes('語')) {
+    // console.log(`No reading found in brackets for ${reading}`);
+    return '';
+  }
+
+  return reading;
 }
