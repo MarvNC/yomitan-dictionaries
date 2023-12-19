2dcdad71bf4f9b2f79695c586aff0208fa78634c
MarvNC <rotrobmin@gmail.com>
2023-12-19 02:31:47 -0800

    Add gdrive updater code

---
+++ gdrive-updater.js
@@ -0,0 +1,122 @@
+const japaneseFolderId = PropertiesService.getScriptProperties().getProperty('japaneseFolderId');
+const mandarinFolderId = PropertiesService.getScriptProperties().getProperty('mandarinFolderId');
+const githubAccessToken = PropertiesService.getScriptProperties().getProperty('githubAccessToken');
+
+const repos = [
+  {
+    url: 'https://api.github.com/repos/stephenmk/Jitendex/releases/latest',
+    folderId: japaneseFolderId,
+    includedNameRegex: /yomi/,
+    removeNameRegex: /jitendex/,
+    fileNamePrefix: '[JA-EN] ',
+  },
+  {
+    url: 'https://api.github.com/repos/themoeway/jmdict-yomitan/releases/latest',
+    folderId: japaneseFolderId,
+    includedNameRegex: /JMnedict/,
+    removeNameRegex: /JMnedict/,
+    fileNamePrefix: '[JA-JA Names] ',
+    addDate: true,
+  },
+  {
+    url: 'https://api.github.com/repos/themoeway/jmdict-yomitan/releases/latest',
+    folderId: japaneseFolderId,
+    includedNameRegex: /KANJIDIC_english/,
+    removeNameRegex: /KANJIDIC_english/,
+    fileNamePrefix: '[Kanji] ',
+    addDate: true,
+  },
+  {
+    url: 'https://api.github.com/repos/MarvNC/cc-cedict-yomitan/releases/latest',
+    folderId: mandarinFolderId,
+    includedNameRegex: /CC\-CEDICT\.(?!Hanzi)/,
+    removeNameRegex: /CC\-CEDICT\.(?!Hanzi)/,
+    fileNamePrefix: '[ZH-EN] ',
+    addDate: true,
+  },
+  {
+    url: 'https://api.github.com/repos/MarvNC/cc-cedict-yomitan/releases/latest',
+    folderId: mandarinFolderId,
+    includedNameRegex: /CC\-CEDICT\.(?=Hanzi)/,
+    removeNameRegex: /CC\-CEDICT\.(?=Hanzi)/,
+    fileNamePrefix: '[Hanzi] ',
+    addDate: true,
+  },
+];
+
+// Function to download jitendex from GitHub and save it to Google Drive
+function downloadFromGithub(githubRepo) {
+  const headers = {
+    Authorization: 'token ' + githubAccessToken,
+  };
+
+  const options = {
+    headers: headers,
+  };
+
+  const releaseInfo = UrlFetchApp.fetch(githubRepo.url, options).getContentText();
+  const releaseData = JSON.parse(releaseInfo);
+
+  const assets = releaseData.assets;
+  let asset;
+
+  // Find the asset containing the includedNameRegex in its name and download it
+  for (let i = 0; i < assets.length; i++) {
+    if (assets[i].name.match(githubRepo.includedNameRegex)) {
+      asset = assets[i];
+      break;
+    }
+  }
+
+  if (asset.browser_download_url !== '') {
+    const response = UrlFetchApp.fetch(asset.browser_download_url);
+    const fileBlob = response.getBlob();
+
+    // Remove existing jitendex files
+    removeFilesWithSubstring(githubRepo.folderId, githubRepo.removeNameRegex);
+
+    const folder = DriveApp.getFolderById(githubRepo.folderId);
+    const createdFile = folder.createFile(fileBlob);
+    // Prepend file with to follow naming convention
+    let fileName = createdFile.getName();
+    // add prefix
+    fileName = githubRepo.fileNamePrefix + fileName;
+
+    // Add date to file name if specified
+    if (githubRepo.addDate) {
+      const date = asset.created_at.split('T')[0];
+      // Suffix before file extension
+      fileName = fileName.replace(/(\.[\w\d_-]+)$/i, ` (${date})$1`);
+    }
+
+    createdFile.setName(fileName);
+
+    Logger.log(`Downloaded ${createdFile.getName()} to Google Drive.`);
+  } else {
+    Logger.log(`No asset containing ${githubRepo.includedNameRegex} found in the latest release.`);
+  }
+}
+
+// Remove existing jitendex files
+/**
+ *
+ * @param {string} folderId
+ * @param {RegExp} regexToRemove
+ */
+function removeFilesWithSubstring(folderId, regexToRemove) {
+  const folder = DriveApp.getFolderById(folderId);
+  const files = folder.getFiles();
+
+  while (files.hasNext()) {
+    const file = files.next();
+    if (file.getName().match(regexToRemove)) {
+      file.setTrashed(true); // Moves file to the trash
+    }
+  }
+}
+
+function downloadAllRepos() {
+  for (const repo of repos) {
+    downloadFromGithub(repo);
+  }
+}
