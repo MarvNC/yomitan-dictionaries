bd701136bfc77c95bc2481fcaed98483a1754f48
MarvNC <rotrobmin@gmail.com>
2023-12-20 16:36:14 -0800

    Update Cifu description and attribution

--- cantonese/cifu/makeCifu.js
+++ cantonese/cifu/makeCifu.js
@@ -79,12 +79,11 @@ async function processCifu() {
     await dict.setIndex({
       title: `Cifu ${type.charAt(0).toUpperCase() + type.slice(1)}`,
       revision: new Date().toISOString().slice(0, 10),
-      description: `Cantonese Frequency List from Cifu.${
+      description: `Cantonese Frequency List from Cifu.\n${
         type === 'spoken'
           ? `Spoken data from HKCanCor (Luke and Wong, 2015), HKCAC (Leung and Law, 2001), CantoMap (Lai and Winterstein, 2019)`
           : `Written data from 3,841 chapters of amateur novels from the website https://www.shikoto.com/`
-      }
-Converted with https://github.com/MarvNC/yomichan-dictionaries`,
+      }\nConverted with https://github.com/MarvNC/yomichan-dictionaries`,
       attribution: `Lai, Regine and Winterstein, Grégoire (2020) "Cifu: a Frequency Lexicon of Hong Kong Cantonese", in Proceedings of The 12th Language Resources and Evaluation Conference, Marseille: European Language Resources Association, p. 3062--3070.`,
       author: `Marv`,
       format: 3,
