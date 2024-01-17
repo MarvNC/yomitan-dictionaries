f124e21955de4094e5e06aec512713a2451b78ac
MarvNC <rotrobmin@gmail.com>
2024-01-17 11:56:47 -0800

    Refactor sort dicts to use regex, also enable/disable based on prefs

--- sort-dictionaries.js
+++ sort-dictionaries.js
@@ -5,157 +5,181 @@ Last Updated 2024-01-16
 
 Instructions:
 Paste this into the browser console of the Yomitan options page and hit enter.
-To match a dictionary by a prefix, set the value of the dictionary to 'prefix' in the order object.
-The order object is in order of priority, so the higher up the dictionary is, the higher the priority.
+Please look at the `profiles` and `groups` objects below to configure the groups
+you want to sort and the order you want them in.
 */
 
 /**
- * Set this to the number of the profile to edit.
+ * Object of profiles with group order and enabled groups.
+ * Your default profile is 1, the next profile is 2, so on.
+ * In my example I have my first profile for JA, the second for ZH, and the third for YUE.
+ * Any groups not in enabledGroups will be disabled.
+ * @type {Record<number, {groupOrder: string[], enabledGroups: string[]}>} profiles
  */
-const profileToEdit = 0;
+const profiles = {
+  1: {
+    groupOrder: ['jafreq', 'ja', 'zhfreq', 'zh', 'yuefreq', 'yue'],
+    enabledGroups: ['jafreq', 'ja', 'zh', 'yue'],
+  },
+  2: {
+    groupOrder: ['zhfreq', 'zh', 'yuefreq', 'yue', 'jafreq', 'ja'],
+    enabledGroups: ['zhfreq', 'zh', 'yue'],
+  },
+  3: {
+    groupOrder: ['yuefreq', 'yue', 'zhfreq', 'zh', 'jafreq', 'ja'],
+    enabledGroups: ['yuefreq', 'yue', 'zh'],
+  },
+};
 
 /**
- * @type {Record<string, '' | 'prefix'>}
+ * Sorted groups of dictionaries' regexes
+ * @type {Record<string, Array<RegExp>>} groups
  */
-// prettier-ignore
-const order = {
-  // JA Frequency
-  'JPDB': '',
-  'Innocent Ranked': '',
-  'Novels': '',
-  'Youtube': '',
-  'BCCWJ-LUW': '',
-  'CC100': '',
-  '青空文庫熟語': '',
-  'Wikipedia': '',
-  
-  // JA Pitch
-  'NHK': '',
-  '大辞泉': '',
-  '新明解第八版': '',
-  '大辞林第四版': '',
-  '三省堂国語辞典第八番': '',
-  
-  // Primary JA-JA
-  '三省堂国語辞典　第八版': '',
-  '新明解国語辞典　第八版': '',
-  '岩波国語辞典　第八版': '',
-  '広辞苑 第七版': '',
-  
-  // JA Differentiation
-  '使い方の分かる 類語例解辞典': '',
-  '漢字ペディア同訓異義': '',
+const groups = {
+  jafreq: [
+    // JA Frequency
+    /^JPDB$/,
+    /^Innocent Ranked$/,
+    /^Novels$/,
+    /^Youtube$/,
+    /^BCCWJ-LUW$/,
+    /^CC100$/,
+    /^青空文庫熟語$/,
+    /^Wikipedia$/,
+  ],
+  ja: [
+    // JA Pitch
+    /^NHK$/,
+    /^大辞泉$/,
+    /^新明解第八版$/,
+    /^大辞林第四版$/,
+    /^三省堂国語辞典第八番$/,
 
-  // Other JA-JA
-  'デジタル大辞泉': '',
-  '旺文社国語辞典 第十一版': '',
-  '国語辞典オンライン': '',
-  '明鏡国語辞典　第二版': '',
-  '大辞林　第四版': '',
-  '新選国語辞典　第十版': '',
-  '精選版　日本国語大辞典': '',
-  
-  // Misc JA-JA
-  '漢検漢字辞典　第二版': '',
-  '漢字源': '',
-  '故事・ことわざ・慣用句オンライン': '',
-  '四字熟語辞典オンライン': '',
-  '新明解四字熟語辞典': '',
-  '学研 四字熟語辞典': '',
-  '実用日本語表現辞典': '',
-  'Pixiv': 'prefix',
-  'JA Wikipedia': 'prefix',
-  '日本語俗語辞書': '',
-  '故事ことわざの辞典': '',
-  '複合語起源': '',
-  'surasura 擬声語': '',
-  '語源由来辞典': '',
-  'weblio古語辞典': '',
-  '全国方言辞典': '',
-  '新語時事用語辞典': '',
+    // Primary JA-JA
+    /^三省堂国語辞典　第八版$/,
+    /^新明解国語辞典　第八版$/,
+    /^岩波国語辞典　第八版$/,
+    /^広辞苑 第七版$/,
 
-  // JA (Rare)
-  '漢字林': '',
-  '福日木健二字熟語': '',
-  '漢字ペディア': '',
-  '全訳漢辞海': '',
-  'KO字源': '',
-  'YOJI-JUKUGO': '',
-  
-  // JA-EN
-  'Jitendex': 'prefix',
-  'NEW斎藤和英大辞典': '',
-  '新和英': '',
-  
-  // JA Names
-  'JMnedict': '',
-  
-  // JA Grammar
-  '日本語文法辞典(全集)': '',
-  '絵でわかる日本語': '',
-  'JLPT文法解説まとめ': '',
-  'どんなときどう使う 日本語表現文型辞典': '',
-  '毎日のんびり日本語教師': '',
-  
-  // JA Kanji Freq
-  'Innocent Corpus Kanji': '',
-  'Wikipedia Kanji': '',
-  '青空文庫漢字': '',
-  'JPDB Kanji Freq': '',
-  
-  // JA Kanji Info
-  '漢字辞典オンライン': '',
-  'KANJIDIC': 'prefix',
-  'JPDB Kanji': '',
-  'mozc Kanji Variants': '',
-  'jitai': '',
-  'TheKanjiMap Kanji Radicals/Composition': '',
-  'Wiktionary漢字': '',
+    // JA Differentiation
+    /^使い方の分かる 類語例解辞典$/,
+    /^漢字ペディア同訓異義$/,
 
-  // YUE Freq
-  'Cifu Spoken': '',
-  'Cifu Written': '',
-  
-  // YUE-EN
-  'CantoDict': '',
-  'Canto CEDICT': '',
-  'Words.hk C-E FS': '',
-  'Words.hk C-C FS': '',
-  'CE Wiktionary': '',
-  'CC-Canto': '',
+    // Other JA-JA
+    /^デジタル大辞泉$/,
+    /^旺文社国語辞典 第十一版$/,
+    /^国語辞典オンライン$/,
+    /^明鏡国語辞典　第二版$/,
+    /^大辞林　第四版$/,
+    /^新選国語辞典　第十版$/,
+    /^精選版　日本国語大辞典$/,
 
-  // ZH Hanzi Info
-  'CC-CEDICT Hanzi [': 'prefix',
-  'ZH Wiktionary Hanzi': '',
-  
-  // ZH Frequency
-  'HSK': '',
-  'BLCUlit': '',
-  'SUBTLEX-CH': '',
-  'BLCUcoll': '',
-  'BLCUmixed': '',
-  'BLCUnews': '',
-  'BLCUsci': '',
-  
-  // ZH-EN
-  'CC-CEDICT [': 'prefix',
-  'Wenlin ABC': '',
-  
-  // ZH-JA
-  '中日大辞典　第二版': '',
-  
-  // ZH-ZH
-  '漢語大詞典': '',
-  '萌典国语辞典': '',
-  '兩岸詞典': '',
-  '牛津英汉汉英词典': '',
-  '五南國語活用辭典': '',
-  '萌典': '',
-  '譯典通英漢雙向字典': '',
-  '现代汉语规范词典': '',
-  '康熙字典': '',
-  '辭源': '',
-  'ZH Wikipedia': 'prefix',
+    // Misc JA-JA
+    /^漢検漢字辞典　第二版$/,
+    /^漢字源$/,
+    /^故事・ことわざ・慣用句オンライン$/,
+    /^四字熟語辞典オンライン$/,
+    /^新明解四字熟語辞典$/,
+    /^学研 四字熟語辞典$/,
+    /^実用日本語表現辞典$/,
+    /^Pixiv.*$/,
+    /^JA Wikipedia.*$/,
+    /^日本語俗語辞書$/,
+    /^故事ことわざの辞典$/,
+    /^複合語起源$/,
+    /^surasura 擬声語$/,
+    /^語源由来辞典$/,
+    /^weblio古語辞典$/,
+    /^全国方言辞典$/,
+    /^新語時事用語辞典$/,
+
+    // JA (Rare)
+    /^漢字林$/,
+    /^福日木健二字熟語$/,
+    /^漢字ペディア$/,
+    /^全訳漢辞海$/,
+    /^KO字源$/,
+    /^YOJI-JUKUGO$/,
+
+    // JA-EN
+    /^Jitendex.*$/,
+    /^NEW斎藤和英大辞典$/,
+    /^新和英$/,
+
+    // JA Names
+    /^JMnedict$/,
+
+    // JA Grammar
+    /^日本語文法辞典\(全集\)$/,
+    /^絵でわかる日本語$/,
+    /^JLPT文法解説まとめ$/,
+    /^どんなときどう使う 日本語表現文型辞典$/,
+    /^毎日のんびり日本語教師$/,
+
+    // JA Kanji Freq
+    /^Innocent Corpus Kanji$/,
+    /^Wikipedia Kanji$/,
+    /^青空文庫漢字$/,
+    /^JPDB Kanji Freq$/,
+
+    // JA Kanji Info
+    /^漢字辞典オンライン$/,
+    /^KANJIDIC.*$/,
+    /^JPDB Kanji$/,
+    /^mozc Kanji Variants$/,
+    /^jitai$/,
+    /^TheKanjiMap Kanji Radicals\/Composition$/,
+    /^Wiktionary漢字$/,
+  ],
+  yuefreq: [
+    // YUE Freq
+    /^Cifu Spoken$/,
+    /^Cifu Written$/,
+  ],
+  yue: [
+    // YUE-EN
+    /^CantoDict$/,
+    /^Canto CEDICT$/,
+    /^Words\.hk C-E FS$/,
+    /^Words\.hk C-C FS$/,
+    /^CE Wiktionary$/,
+    /^CC-Canto$/,
+  ],
+  zhfreq: [
+    // ZH Frequency
+    /^HSK$/,
+    /^BLCUlit$/,
+    /^SUBTLEX-CH$/,
+    /^BLCUcoll$/,
+    /^BLCUmixed$/,
+    /^BLCUnews$/,
+    /^BLCUsci$/,
+  ],
+  zh: [
+    // ZH-EN
+    /^CC-CEDICT(?! Hanzi).*$/,
+    /^Wenlin ABC$/,
+
+    // ZH-JA
+    /^中日大辞典　第二版$/,
+
+    // ZH-ZH
+    /^漢語大詞典$/,
+    /^萌典国语辞典$/,
+    /^兩岸詞典$/,
+    /^牛津英汉汉英词典$/,
+    /^五南國語活用辭典$/,
+    /^萌典$/,
+    /^譯典通英漢雙向字典$/,
+    /^现代汉语规范词典$/,
+    /^康熙字典$/,
+    /^辭源$/,
+    /^ZH Wikipedia.*$/,
+
+    // ZH Hanzi Info
+    /^CC-CEDICT Hanzi.*$/,
+    /^ZH Wiktionary Hanzi$/,
+  ],
 };
 
 import('./js/pages/settings/settings-controller.js').then(async (SettingsController) => {
@@ -164,78 +188,112 @@ import('./js/pages/settings/settings-controller.js').then(async (SettingsControl
    */
   const settingsController = new SettingsController.SettingsController();
 
-  settingsController.profileIndex = profileToEdit;
-
   /**
-   * @type {import('./types/ext/settings').ProfileOptions} ProfileOptions
+   * @type {RegExp[]} failedToMatchDicts
    */
-  const currentOptions = await settingsController.getOptions();
-  const { dictionaries } = currentOptions;
+  const failedToMatchDicts = [];
 
-  const originalLength = dictionaries.length;
+  for (const [profile, { groupOrder, enabledGroups }] of Object.entries(profiles)) {
+    console.log(`Sorting dictionaries for profile ${profile}...`);
+    settingsController.profileIndex = profile;
 
-  const newDictionaries = [];
+    /**
+     * @type {import('./types/ext/settings').ProfileOptions} ProfileOptions
+     */
+    let currentOptions;
+    try {
+      currentOptions = await settingsController.getOptions();
+    } catch (error) {
+      console.error(`Failed to get options for profile ${profile}!`);
+      console.error(error);
+      continue;
+    }
+    const { dictionaries } = currentOptions;
 
-  for (const [key, val] of Object.entries(order)) {
-    let foundCount = 0;
+    const originalLength = dictionaries.length;
+
+    const newDictionaries = [];
+
+    for (const group of groupOrder) {
+      const groupEnabled = enabledGroups.includes(group);
+      for (const dictRegex of groups[group]) {
+        let foundCount = 0;
+        // Find the dictionaries in dictionaries that match the regex
+        const matchedDictionaries = dictionaries.filter((dict) => {
+          if (!dict) return false;
+          return dictRegex.test(dict.name);
+        });
+        if (matchedDictionaries.length > 1) {
+          console.warn(`Found more than one dictionaries for ${dictRegex}`);
+        }
+        if (matchedDictionaries.length === 0) {
+          failedToMatchDicts.push(dictRegex);
+        }
+        // Add the dictionaries to the newDictionaries array
+        for (const dict of matchedDictionaries) {
+          dict.enabled = groupEnabled;
+          newDictionaries.push(dict);
+          foundCount++;
+          const index = dictionaries.indexOf(dict);
+          delete dictionaries[index];
+        }
+        // for (const dict of dictionaries) {
+        //   if (!dict) continue;
+        //   const dictName = dict.name;
+        //   let matched = false;
+
+        //   // try to match regex to dict name
+        //   if (dictRegex.test(dictName)) {
+        //     matched = true;
+        //   }
+
+        //   if (matched) {
+        //     newDictionaries.push(dict);
+        //     foundCount++;
+        //     const index = dictionaries.indexOf(dict);
+        //     delete dictionaries[index];
+        //   }
+        // }
+        // if (foundCount > 1) {
+        //   console.warn(`Found more than one dictionaries for ${dictRegex}`);
+        // }
+        // if (foundCount === 0) {
+        //   console.log(`Did not find dictionary: ${dictRegex}`);
+        // }
+      }
+    }
+
+    // Get the remaining dictionaries
     for (const dict of dictionaries) {
-      if (!dict) continue;
-      const name = dict.name;
-      let matched = false;
-
-      switch (val) {
-        case 'prefix':
-          if (name.startsWith(key)) {
-            matched = true;
-          }
-          break;
-        default:
-          if (name === key) {
-            matched = true;
-          }
-          break;
-      }
-
-      if (matched) {
+      if (dict) {
+        console.warn(`Found dictionary not in sort order: ${dict.name}`);
         newDictionaries.push(dict);
-        foundCount++;
-        const index = dictionaries.indexOf(dict);
-        delete dictionaries[index];
       }
     }
-    if (foundCount > 1) {
-      console.warn(`Found more than one dictionaries for ${key}`);
-    }
-    if (foundCount === 0) {
-      console.log(`Did not find dictionary: ${key}`);
+
+    if (originalLength !== newDictionaries.length) {
+      console.error(`Dictionaries length mismatch! Aborting sort...`);
+    } else {
+      // Go through order and set the priority descending
+      let priority = newDictionaries.length * 10;
+      for (const dict of newDictionaries) {
+        dict.priority = priority;
+        priority -= 10;
+      }
+
+      await settingsController.modifyProfileSettings([
+        {
+          action: 'set',
+          path: 'dictionaries',
+          value: newDictionaries,
+        },
+      ]);
+      console.log(`Updated dictionaries order!`);
     }
   }
 
-  // Get the remaining dictionaries
-  for (const dict of dictionaries) {
-    if (dict) {
-      console.warn(`Found dictionary not in sort order: ${dict.name}`);
-      newDictionaries.push(dict);
-    }
-  }
-
-  if (originalLength !== newDictionaries.length) {
-    console.error(`Dictionaries length mismatch! Aborting sort...`);
-  } else {
-    // Go through order and set the priority descending
-    let priority = newDictionaries.length * 10;
-    for (const dict of newDictionaries) {
-      dict.priority = priority;
-      priority -= 10;
-    }
-
-    await settingsController.modifyProfileSettings([
-      {
-        action: 'set',
-        path: 'dictionaries',
-        value: newDictionaries,
-      },
-    ]);
-    console.log(`Updated dictionaries order!`);
+  if (failedToMatchDicts.length > 0) {
+    console.log(`Failed to match dictionaries:`);
+    console.log(failedToMatchDicts);
   }
 });
